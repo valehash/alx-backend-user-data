e37b6a29afe26950ae47228f3a0c343855e419b2
vale-hash <newlechim@gmail.com>
2024-09-03 19:37:41 +0100

    task 7 b64 encoding

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,16 @@ class BasicAuth(Auth):
     """ The authentication class for all paths,
     Inherits from auth.py
     """
+    def extract_base64_authorization_header
+    (self, authorization_header: str) -> str:
+        """Function that encodes the autohrization header
+        params:(self, auth_header: str)
+        returns: str
+        """
+        if authorization_header is None:
+            return None
+        if isinstance(authorization_header, str):
+            if authorization_header.startswith("Basic "):
+                return authorization_header.split(" ")[1:][0]
+            return None
+        return None
