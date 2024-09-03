3486ff33773d41915304b0dbcb5c22d5a403c0ae
vale-hash <newlechim@gmail.com>
2024-09-03 14:09:52 +0100

    Task3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,21 +5,15 @@ Auth model for the api
 from typing import *
 from flask import request
 
+
 class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """returns path that requre auth"""
-        if path==None:
-            return True
-        
-        if excluded_paths==None:
+        if path == None:
             return True
 
-        normalized_path= path if path.endswith("/") else path+"/"
-
-        if normalized_path in excluded_paths:
-            return False
-        return True
-
+        if excluded_paths == None:
+            return True
     def authorization_header(self, request=None) -> str:
         """returns the flask object"""
         return None
