a4b1256d7b968dedebff4ad17cfb57f238dbe7cf
vale-hash <newlechim@gmail.com>
2024-09-03 14:37:46 +0100

    documented auth class

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,19 +7,31 @@ from flask import request
 
 
 class Auth:
-    """ The authentication class for all paths
+    """
+    The authentication class for all paths,
+    it handles which paths need authtication
+    and which ones do not
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """returns path that requre auth"""
-        if path == None:
+        """returns path that requre auth
+        input: path:str excluded_paths:List[str]
+        returns:bool
+        """
+        if path is None:
+            return True
+        if excluded_paths is None:
             return True
 
-        if excluded_paths == None:
-            return True
     def authorization_header(self, request=None) -> str:
-        """returns the flask object"""
+        """returns the flask object
+        input:request
+        returns:str
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Returns the current user"""
+        """Returns the current user
+        input:request
+        returns:TypeVar
+        """
         return None
