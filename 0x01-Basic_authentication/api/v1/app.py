d9be082be1b3e1cd33da71be1fc2606c8cae4721
vale-hash <newlechim@gmail.com>
2024-09-03 13:42:10 +0100

    autopepped

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -20,17 +20,20 @@ def not_found(error) -> str:
     """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
 def not_authorized(error) -> str:
     """ Unuathorized error handler
     """
-    return jsonify({"error": "Unauthorized"}), 401 
+    return jsonify({"error": "Unauthorized"}), 401
+
 
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ forbidden request error handler
     """
-    return jsonify({"error": "Forbidden"}),403
+    return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,18 +25,20 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/unauthorized/', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """Get /api/v1/unauthorized
     Return:
-	-unathorized status
+        -unathorized status
     """
     return abort(401)
 
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+
+@app_views.route('/forbidden/', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """Get /api/v1/forbidden
     Return:
-	-forbidden status
+        -forbidden status
     """
     return abort(403)
