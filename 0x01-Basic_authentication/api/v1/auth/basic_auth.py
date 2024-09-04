d0f425488c44eae5ccd3941e7adf245298dd0150
vale-hash <newlechim@gmail.com>
2024-09-04 03:25:39 +0100

    basic auth decode and encode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """
 Basic Auth model for the api
 """
+import base64
 from .auth import Auth
 from typing import List, TypeVar
 from flask import request
@@ -11,6 +12,7 @@ class BasicAuth(Auth):
     """ The authentication class for all paths,
     Inherits from auth.py
     """
+
     def extract_base64_authorization_header(
             self, authorization_header: str) -> str:
         """Function that encodes the autohrization header
@@ -23,4 +25,20 @@ class BasicAuth(Auth):
             if authorization_header.startswith("Basic "):
                 return authorization_header.split(" ")[1:][0]
             return None
+        return None
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """"Function that decodes the  base64 string
+        params(self, base64_aurization heaser : str)
+        returns: str
+        """
+        if base64_authorization_header is None:
+            return None
+        if isinstance(base64_authorization_header, str):
+            try:
+                decoded_mess = base64.b64decode(base64_authorization_header)
+                return decoded_mess.decode("utf-8")
+            except (base64.binascii.Error, UnicodeDecodeError):
+                return None
         return None
