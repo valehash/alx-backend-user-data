08f9fae94376cd847cbc9197936529bc7941931c
vale-hash <newlechim@gmail.com>
2024-09-09 13:03:37 +0100

    remove users/me route

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -122,15 +122,15 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
+"""
 @app_views.route('/users/me', methods=['GET'], strict_slashes=False)
 def view_me() -> str:
-    """ GET /api/v1/users/me
-    Path parameter:
-    Return:
-      - User object JSON represented
-      - 404 if the User ID doesn't exist
-    """
+  #GET /api/v1/users/me
+   # Path parameter:
+    #Return:
+     # - User object JSON represented
+     # - 404 if the User ID doesn't exist
     if request.current_user is None:
         abort(404)
     return jsonify(request.current_user.to_json())
+    """
