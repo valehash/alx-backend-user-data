36a57c7aaaeff246fcdb636cb84430e1bb3d586b
vale-hash <newlechim@gmail.com>
2024-09-11 13:12:15 +0100

    user_auth begins

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+User declaration file
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Base class for declarative class definitions
+Base = declarative_base()
+
+class User(Base):
+    """A user class with the properties created in sqlachemy"""
+    __tablename__ = 'users'
+    
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String, nullable=False, unique=True)
+    hashed_password = Column(String, nullable=False)
+    session_id = Column(String, nullable=True)
+    reset_token = Column(String, nullable=True)
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email})>"
