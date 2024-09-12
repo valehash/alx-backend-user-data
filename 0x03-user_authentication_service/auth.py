d5414c5d7bb8f4b1577adda54604614aeb09563c
vale-hash <newlechim@gmail.com>
2024-09-12 13:30:16 +0100

    env/python3

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,10 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Authorization file
 """
+
 import bcrypt
 
+
 def _hash_password(pwd:str) -> bytes:
+    """Function to create a hashed password from a string"""
     pwd = pwd.encode('utf-8')
     salt = bcrypt.gensalt()
     hash_password =  bcrypt.hashpw(pwd, salt)
