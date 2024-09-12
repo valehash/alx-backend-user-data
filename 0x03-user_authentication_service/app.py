cf896bce19e18931a2b3cbf0bd54283a04aaa79d
vale-hash <newlechim@gmail.com>
2024-09-12 18:06:06 +0100

    task 6 done

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,14 @@
+from flask import Flask,jsonify,request
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"], strict_slashes=False)
+def start() -> str:
+    """
+     single GET route ("/") and use flask.jsonify 
+     to return a JSON payload of the form:
+    """
+    return jsonify({"message": "Bienvenue"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
