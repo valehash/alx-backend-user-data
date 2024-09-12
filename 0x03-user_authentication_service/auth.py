718cf2e9c9dba748ce8a21e423368e1b4b5d633f
vale-hash <newlechim@gmail.com>
2024-09-12 17:55:47 +0100

    task 5 done

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -4,17 +4,17 @@ Authorization file
 """
 import bcrypt
 from db import DB
-from user import User 
+from user import User
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
-def _hash_password(pwd:str) -> bytes:
+
+def _hash_password(pwd: str) -> bytes:
     """Function to create a hashed password from a string"""
     pwd = pwd.encode('utf-8')
     salt = bcrypt.gensalt()
-    hash_password =  bcrypt.hashpw(pwd, salt)
+    hash_password = bcrypt.hashpw(pwd, salt)
     return hash_password
-
 
 
 class Auth:
@@ -24,11 +24,19 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-    def register_user(self, email:str, pwd:str)-> User:
+    def register_user(self, email: str, pwd: str) -> User:
+        """
+        Register user adds a new user if the email does not exist yet
+        args:
+            email: str - the users email
+            pwd: str - regular password the user inputs
+        returns:
+            returns A user objext
+        """
         try:
             self._db.find_user_by(email=email)
         except NoResultFound:
-            passw= _hash_password(pwd)
+            passw = _hash_password(pwd)
             user = self._db.add_user(email, passw)
             return user
-        raise ValueError (f"user {email} already exists")
+        raise ValueError(f"user {email} already exists")
